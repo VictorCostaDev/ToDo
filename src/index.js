f090164219021572434bf516b5419403bbb7f983
Victor Gabriel <victo091198@gmail.com>
2022-10-14 17:59:29 -0300

    Funcionalidade de atualizar uma task foi adicionada

--- src/index.js
+++ src/index.js
@@ -52,7 +52,18 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+
+  user.todos.forEach( todo => {
+    if (todo.id === id) {
+      todo.title = title;
+      todo.deadline = deadline
+    }
+  });
+
+  response.status(200).json({"message": "successfully updated"});
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
